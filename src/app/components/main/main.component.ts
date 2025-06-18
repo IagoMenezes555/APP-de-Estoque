f9f44d787708fb4619a3c511277688e750710500
Iago <iagomenezes555@gmail.com>
2025-06-18 11:01:07 -0300

    feat: list

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { IonTitle, IonButton, IonContent, IonModal, IonInput } from "@ionic/angular/standalone";
+import { IonTitle, IonButton, IonContent, IonModal, IonInput, IonList, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from "@ionic/angular/standalone";
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   templateUrl: './main.component.html',
-  imports: [IonInput, IonModal, IonContent, IonTitle, IonButton, FormsModule],
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonList, IonInput, IonModal, IonContent, IonTitle, IonButton, FormsModule, CommonModule],
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent  implements OnInit {
@@ -26,6 +27,21 @@ export class MainComponent  implements OnInit {
 
   closeModal(){
     this.modal = false;
+  }
+
+  addItem(){
+    const newItem = {
+      name: this.item.name,
+      amount: this.item.amount
+    }
+
+    this.stock.push(newItem);
+
+    localStorage.setItem("stock", JSON.stringify(this.stock));
+
+    this.closeModal();
+
+    console.log(this.stock)
   }
 
   constructor() { }
