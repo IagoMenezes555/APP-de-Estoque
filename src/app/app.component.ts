6a0fef3104e62ceb404e5ca364a0df1a495c70ac
Iago <iagomenezes555@gmail.com>
2025-06-19 13:04:48 -0300

    feat: apk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar } from '@capacitor/status-bar';
 
+StatusBar.setOverlaysWebView({ overlay: false });
+StatusBar.setBackgroundColor({ color: '#570082' });
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
